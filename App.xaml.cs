6f4790e0773d05cc97d5e2dfc7713ffa10eba4a1
bigbug <ibigbugcc@gmail.com>
2024-08-23 18:47:17 +0800

    Init

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Hyperv_Tools
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
